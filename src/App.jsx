349a0412bdf7b1f39a49898ea2c73a3fa1712213
Manish Patidar <144804537+ManishPatidar806@users.noreply.github.com>
2024-03-16 10:04:08 +0000

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
-import Center from './Components/Center'
-import SideBar from './Components/SideBar'
 import Mainbox from './Components/Mainbox'
+import './App.css'
+
+
 function App() {
   return (
-    <>
-<SideBar/>
-<Center/>
-    </>
+    <Mainbox/>
   )
 }
 
--- src/Components/Mainbox.jsx
+++ src/Components/Mainbox.jsx
@@ -1,11 +1,97 @@
-import Center from "./Center";
-import "./Mainbox.css"
 import SideBar from "./SideBar";
-export default function Mainbox(){
-    return (
-      <div className="MainBox">
-        <SideBar/>
-        <Center/>
+import React, { useContext } from "react";
+import { assets } from "./../assets/assets"
+import { Context } from "../context/context";
+import Markdown from 'react-markdown'
+import "./Mainbox.css"
+
+export default function Mainbox() {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setinput,
+    input
+
+  } = useContext(Context);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSent();
+  }
+
+  return (
+    <div className="MainBox">
+      <SideBar />
+      <div className="Center Center-container">
+        <div className="nav">
+          <img className="logo" src={assets.name_logo_icon} alt="" />
+          <img className="user" src={assets.user_icon} alt="" />
+        </div>
+        <div className="result-container">
+        {!showResult ? <>
+          <div className="greet">
+            <p ><span>Hello,Manish.</span></p>
+            <p>How can i help you today?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Suggest beautiful places to see on an upcoming road trip</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Create a PowerPoint Presentation on Evolution of Ai.</p>
+              <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Create a beautiful message for wishing birthday.</p>
+              <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Write a Programme to Find Largest elements in Array.</p>
+              <img src={assets.code_icon} alt="" />
+            </div>
+          </div>
+        </> :
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} alt="" />
+              <div>{recentPrompt}</div>
+            </div>
+            <div className="result-data">
+              <img src={assets.logo_icon} alt="" />
+              {loading ?
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+                : <Markdown>{resultData}</Markdown>
+              }
+            </div>
+          </div>
+        }
+        </div>
+
+        <div className="main-bottom">
+          <form className="search-box" onSubmit={handleSubmit}>
+            <input onChange={(e) => setinput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
+            <div className="imagesection">
+              <img src={assets.mic_icon} alt="" />
+              {input ?
+                <button type="submit" className="submit-btn">
+                  <img src={assets.send_icon} alt="" />
+                </button> : null}
+            </div>
+          </form>
+          <p className="bottom-info">
+            Chat-bot is work according to their store data
+          </p>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
